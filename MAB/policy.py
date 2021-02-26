4b639a717d1b3a2b95fc57663f79bc89b471464d
dbsxodud11 <99yty@kaist.ac.kr>
2021-02-26 20:56:27 +0900

    add policy

--- MAB/policy.py
+++ MAB/policy.py
@@ -18,4 +18,23 @@ class EpsGreedyPolicy(Policy) :
         else :
             action = np.argmax(Qs)
         return action
-    
+
+class GreedyPolicy(EpsGreedyPolicy) :
+    def __init__(self) :
+        EpsGreedyPolicy.__init__(self, eps=0)
+
+class RandomPolicy(EpsGreedyPolicy) :
+    def __init__(self) :
+        EpsGreedyPolicy.__init__(self, eps=1)
+
+class UCBPolicy(Policy) :
+    def __init__(self, c_level) :
+        self.c_level = c_level
+
+    def get_action(self, Qs, Ns) :
+        exploration = np.log(np.sum(Ns)+1) / Ns
+        exploration = self.c_level*np.sqrt(exploration)
+        
+        new_Qs = Qs + exploration
+        action = np.argmax(new_Qs)
+        return action
